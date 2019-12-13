2e33a9d5c82e9113f9ba77a405ce1d3fd5c6df20
y1z <whyis2cool@gmail.com>
2019-12-12 23:58:08 -0600

    add more ways to interpert the time
    add function the convert the defualt return type to other return types

--- TimerReborn/Timer.cpp
+++ TimerReborn/Timer.cpp
@@ -3,10 +3,9 @@
 #include <chrono>
 
 Timer::Timer()
-{
+{	// this init this value 
 	m_Result = std::chrono::seconds(0);
 }
-
 
 Timer::~Timer()
 {
@@ -23,8 +22,7 @@ void Timer::EndTiming()
 
 	m_Result = (m_End - m_Start);
 
-
-	PrintResult();
+	//PrintResult();
 }
 
 void Timer::PrintResult()
@@ -37,11 +35,35 @@ void Timer::PrintResult()
 	ConverstionNano = std::chrono::duration_cast<std::chrono::nanoseconds>(m_Result);
 	ConverstionSeconds = std::chrono::duration_cast<TimeMeasurementSeconds>(m_Result);
 
-	printf("------------(TIMES)--------------\n");
+	printf("\n------------(TIMES)--------------\n");
 	std::cout << "PicoSeconds = [" << m_Result.count() << "] \n";
-
 	std::cout << "NanoSeconds = [" << ConverstionNano.count() << "] \n";
-	std::cout << "Seconds = [" << ConverstionSeconds.count() << "] \n\n ";
-	printf("--------------------------------\n");
+	std::cout << "Seconds = [" << ConverstionSeconds.count() << "] \n ";
+	printf("--------------------------------\n\n");
 
+}
+
+uint64_t Timer::GetResult()
+{
+	return m_Result.count();
+}
+
+uint64_t Timer::GetResultMicroSeconds()
+{
+	std::chrono::microseconds ElapsedTime = std::chrono::duration_cast<std::chrono::microseconds>(m_Result);
+
+	return ElapsedTime.count();
+}
+
+uint64_t Timer::GetResultMiliSeconds()
+{
+	std::chrono::milliseconds Milisecons = std::chrono::duration_cast<std::chrono::milliseconds>(m_Result);
+	return Milisecons.count();
+}
+
+double Timer::GetResultSeconds()
+{
+	TimeMeasurementSeconds ConverstionSeconds = std::chrono::duration_cast<TimeMeasurementSeconds>(m_Result);
+
+	return ConverstionSeconds.count();
 }
--- TimerReborn/Timer.h
+++ TimerReborn/Timer.h
@@ -1,13 +1,16 @@
 #pragma once
 #include <chrono>
 #include <cinttypes>
-
+/*!
+ * \author Yhaliff said Barraza Zubia 
+ * \brief measures time and prints to the screen can also receive the time 
+ elapsed in other time measurement.
+ **/
 class Timer
 {
 	/*! used for measuring durations in terms of picoseconds*/
 	using TimeMeasurementPico = std::chrono::duration<uint64_t, std::pico>;
-
-	/*! this duration that can have a decimal point
+	/*! this duration has seconds that can have a decimal point
 	(just because I get confused with other Representation)*/
 	using TimeMeasurementSeconds = std::chrono::duration<double, std::ratio<1, 1>>;
 
@@ -21,6 +24,10 @@ public: // functions
 
 	void StartTiming();
 	void EndTiming();
+	uint64_t GetResult();
+	uint64_t GetResultMicroSeconds();
+	uint64_t GetResultMiliSeconds();
+	double GetResultSeconds();
 private:
 	void PrintResult();
 
